1f3b2002fc006ad9b9c0e3c5c01a5432ef018160
Isaac Hernandez <isaac.rhz@gmail.com>
2020-11-23 17:43:26 -0600

    Commii initial

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Isaac Hernandez on 23/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
