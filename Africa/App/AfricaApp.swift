602d04da9cf8ed05f438d684125a7cb03ee4e1d8
Isaac Hernandez <isaac.rhz@gmail.com>
2020-11-23 18:41:42 -0600

    Agregar imagenes, colores y configuración inicial

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,19 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Isaac Hernandez on 23/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
+
+
---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Isaac Hernandez on 23/11/20.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
